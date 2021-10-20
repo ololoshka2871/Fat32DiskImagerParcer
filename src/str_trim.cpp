1e65784637ef1efddfebd7a9196e5882a515cfd9
ololoshka2871 <SweetTreasure@2ch.hk>
2021-10-20 09:05:58 +0300

    add str_trim

---
+++ src/str_trim.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <cctype>
+#include <functional>
+#include <locale>
+
+// trim from start (in place)
+void ltrim(std::string &s) {
+  s.erase(s.begin(),
+          std::find_if(s.begin(), s.end(),
+                       std::not1(std::ptr_fun<int, int>(std::isspace))));
+}
+
+// trim from end (in place)
+void rtrim(std::string &s) {
+  s.erase(std::find_if(s.rbegin(), s.rend(),
+                       std::not1(std::ptr_fun<int, int>(std::isspace)))
+              .base(),
+          s.end());
+}
+
+// trim from both ends (in place)
+void trim(std::string &s) {
+  ltrim(s);
+  rtrim(s);
+}
+
+// trim from start (copying)
+std::string ltrim_copy(std::string s) {
+  ltrim(s);
+  return s;
+}
+
+// trim from end (copying)
+std::string rtrim_copy(std::string s) {
+  rtrim(s);
+  return s;
+}
+
+std::string trim_copy(std::string s) {
+  rtrim(s);
+  ltrim(s);
+  return s;
+}
---
+++ src/str_trim.h
@@ -0,0 +1,21 @@
+
+#pragma once
+
+#include <string>
+
+// trim from start (in place)
+void ltrim(std::string &s);
+
+// trim from end (in place)
+void rtrim(std::string &s);
+
+// trim from both ends (in place)
+void trim(std::string &s);
+
+// trim from start (copying)
+std::string ltrim_copy(std::string s);
+
+// trim from end (copying)
+std::string rtrim_copy(std::string s);
+
+std::string trim_copy(std::string s);
